e6a84fae02932c7cc09a05bc7ca5e287bdb5730f
Su Wei <suwei.thien#gmail.com>
2019-03-27 23:54:06 +0800

    Add Lyn_ionic Directory

---
+++ lynda-ionic/todoApp/src/pages/home/home.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+
+import { NavController, AlertController, reorderArray } from 'ionic-angular';
+
+import { TodoProvider } from '../../providers/todo/todo';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  public todos = [];
+  public reorderIsEnabled = false;
+
+  constructor(private todoProvider: TodoProvider, public navCtrl: NavController, private alertController: AlertController) {
+    this.todos = this.todoProvider.getTodos();
+  }
+
+  toggleReorder(){
+   this.reorderIsEnabled = !this.reorderIsEnabled;
+  }
+
+  itemReordered($event){
+   reorderArray(this.todos, $event);
+  }
+
+  openTodoAlert(){
+    let addTodoAlert = this.alertController.create({
+      title: "Add A Todo",
+      message: "Enter Your Todo",
+      inputs: [
+        {
+          type: "text",
+          name: "addTodoInput"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel"
+        },
+        {
+          text: "Add Todo",
+          handler: (inputData)=> {
+            let todoText;
+            todoText = inputData.addTodoInput;
+            this.todoProvider.addTodo(todoText);
+          }
+        }
+      ]
+    });
+    addTodoAlert.present();
+
+  }
+
+}
---
+++ lynda-ionic/todoApp/src/providers/todo/todo.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the TodoProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class TodoProvider {
+  private todos = [];
+
+  constructor(public http: Http) {
+    console.log('Hello TodoProvider Provider');
+  }
+
+  getTodos(){
+    return this.todos;
+  }
+
+  addTodo(todo){
+    this.todos.push(todo);
+  }
+
+}
